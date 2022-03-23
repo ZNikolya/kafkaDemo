3418858c38042c5ac260e55be8db38d644531b73
Admin <nikozohrabyan@mail.ru>
2022-03-23 20:14:17 +0400

    kafkaDemo

---
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+
+import lombok.*;
+
+@Data
+public class UserDto {
+    private Long age;
+    private String name;
+    private Address address;
+}
--- src/main/java/com/example/demo/restController/MsgController.java
+++ src/main/java/com/example/demo/restController/MsgController.java
@@ -2,6 +2,8 @@ package com.example.demo.restController;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MsgController {
 
     @Autowired
-    private KafkaTemplate<String,String> kafkaTemplate;
+    public KafkaTemplate<String, String> kafkaTemplate;
 
     @PostMapping
-    public void sendOrder(String msgId, String msg){
-        kafkaTemplate.send("msg",msgId,msg);
+    public void sendOrder(String msgId, String msg) {
+        kafkaTemplate.send("msg", msgId, msg);
+    }
+
+    @PostMapping
+    public void sendMsg(String msgId, String msg) {
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("msg", msgId, msg);
+        future.addCallback(System.out::println, System.err::println);
+        kafkaTemplate.flush();
     }
 }
